b4976b36713fdf4417c74fd4a36acf4ca4d66c9b
archanray <ray@cs.umass.edu>
2024-03-24 18:34:35 -0400

    lanczos

--- src/lanczos.py
+++ src/lanczos.py
@@ -1,7 +1,7 @@
 import numpy as np
 from copy import deepcopy
 
-def lanczos(A, v, k):
+def lanczos(A, v, k, return_type="T"):
     """
     implements lanczos algorithm to return T and not the Q vectors
     
@@ -13,25 +13,84 @@ def lanczos(A, v, k):
     # init variables
     n = len(A)
     T = np.zeros((k,k))
+    Q = np.zeros((n,k))
     
     # set up
-    Q = v / np.linalg.norm(v)
+    Q[:,0] = v / np.linalg.norm(v)
     alpha = np.dot(Q.T, np.dot(A, Q))
     Q_tilde = np.dot(A, Q) - alpha*Q
+    # orthogonalize Q_tilde
+    Q_tilde = Q_tilde - np.dot(np.dot(Q, Q.T), Q_tilde)
     T[0,0] = alpha
     
     # iteration
     for i in range(1,k):
         eta = np.linalg.norm(Q_tilde)
         # note there is a modification in the following line for local orthogonalization 
-        alpha = np.dot(Q_tilde.T, np.dot(A, Q_tilde)) / (eta ** 2) - np.dot(Q_tilde.T, Q)
+        Q[:,i] = Q_tilde / eta
+        alpha = 
         Q_tilde = (np.dot(A, Q_tilde / eta)) - (alpha / eta)*Q_tilde - eta*Q
         Q = Q_tilde / eta
         
         # Set variable
         T[i,i] = alpha
         T[i, i-1] = T[i-1, i] = eta
+
+        if return_type == "Q" or return_type == "QT":
+            approx_eigvecs[:, i] = Q
         
-    #     print("alpha, eta:", alpha, eta)
-    # print("*******************************EOL**************************************************")
-    return
+    if return_type == "T":
+        return T
+    elif return_type == "Q":
+        return Q
+    else:
+        return Q, T
+    
+def exact_lanczos(A,q0,k,reorth=True, return_type="QT"):
+    """
+    run Lanczos with reorthogonalization
+    
+    Input
+    -----
+    A : entries of diagonal matrix A
+    q0 : starting vector
+    k : number of iterations
+    B : entries of diagonal weights for orthogonalization
+    """
+    
+    n = len(q0)
+    
+    Q = np.zeros((n,k),dtype=A.dtype)
+    a = np.zeros(k,dtype=A.dtype)
+    b = np.zeros(k-1,dtype=A.dtype)
+    
+    if return_type == "QT":
+        T = np.zeros((k,k))
+    
+    Q[:,0] = q0 / np.sqrt(q0.T@q0)
+    
+    for i in range(1,k+1):
+        # expand Krylov space
+        qi = A@Q[:,i-1] - b[i-2]*Q[:,i-2] if i>1 else A@Q[:,i-1]
+        
+        a[i-1] = qi.T@Q[:,i-1]
+        qi -= a[i-1]*Q[:,i-1]
+        
+        if return_type == "QT":
+            T[i-1,i-1] = a[i-1]
+        
+        if reorth:
+            qi -= Q@(Q.T@qi) # regular GS
+            #for j in range(i-1): # modified GS (a bit too slow)
+            #    qi -= (qi.T@Q[:,j])*Q[:,j]
+            
+        if i < k:
+            b[i-1] = np.sqrt(qi.T@qi)
+            Q[:,i] = qi / b[i-1]
+            
+            if return_type == "QT":
+                T[i-1,i] = T[i,i-1] = b[i-1]
+    if return_type == "QT":            
+        return Q, T
+    else:
+        return Q
--- unittests.py
+++ unittests.py
@@ -1,1 +1,20 @@
 import numpy as np
+from src.lanczos import exact_lanczos
+from src.lanczos import lanczos
+import unittest
+
+class TestCalculations(unittest.TestCase):
+    def test_lanczos(self):
+        n = 20
+        A = np.random.randn(n,n)
+        v = np.random.randn(n)
+        k = n
+        Q1, T1 = lanczos(A, v, k, return_type="QT")
+        Q2, T2 = exact_lanczos(A, v, k, return_type="QT")
+        error1 = np.linalg.norm(Q1@T1@Q1.T - A)
+        error2 = np.linalg.norm(Q2@T2@Q2.T - A)
+        print(error1, error2)
+        self.assertTrue(error1 == error2, 'Matrices are unequal.')
+
+if __name__ == '__main__':
+    unittest.main()
