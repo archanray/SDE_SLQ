fcbcc3a07fc900f9acf009edd1a3b5f248592ed5
Archan Ray <ray@swarm.cs.umass.edu>
2024-06-28 20:59:14 -0400

    ray

--- run_swarm.sh
+++ run_swarm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes 6
-#SBATCH --cpus-per-task 12
+#SBATCH --cpus-per-task 6
 #SBATCH -p longq
-#SBATCH --mem 32000
+#SBATCH --mem 3200
 #SBATCH -t 07-00:00:00  # Job time limit
-#SBATCH -o main.out
-#SBATCH -e main.err
+#SBATCH -o ./main.out
+#SBATCH -e ./main.err
 
 python main.py
